42d7ca4f3b1deec06b5671f2eafd8318eaa9e954
Mathieu Dugré <math.dugre@gmail.com>
2019-08-19 15:36:45 -0400

    Finished the reducer

--- reducer.sh
+++ reducer.sh
@@ -1,80 +1,90 @@
 #!/bin/bash
 
 # Parse arguments
-while getopts "i:o:c:p:" opt; do
+while getopts "hi:o:c:p:" opt; do
     case $opt in
         i) INPUT_IMG=$OPTARG    ;;
-        o) OUTPUT_IMG=$OPTARG   ;;
         c) COMMAND_FILE=$OPTARG ;;
-        p) PRUNED_DIR=$OPTARG   ;;
-        *) echo "fatal error: ${OPTARG} invalid arguments" >&2
+        *) echo "usage: sh reducer.sh -i <INPUT_IMG> -c <COMMAND_FILE>" 
            exit 1
     esac
 done
 
-if [ -z $INPUT_IMG ]; then
+if [ -z ${INPUT_IMG} ]; then
     echo 'fatal error: Input image is required' >&2
     exit 1
 fi
 
-if [ -z $OUTPUT_IMG ]; then
-    echo 'fatal error: Output image is required' >&2
-    exit 1
-fi
-
-if [ -z $COMMAND_FILE ]; then
+if [ -z ${COMMAND_FILE} ]; then
     echo 'fatal error: Path to the command file is required' >&2
     exit 1
-elif [ ! -f "$COMMAND_FILE" ]; then
+elif [ ! -f "${COMMAND_FILE}" ]; then
     echo 'fatal error: Command file must be a file' >&2
     exit 1
 fi
 
-if [[ ! -f "$PRUNED_DIR" ]]; then
-    echo 'fatal error: Pruned needs to be a file with listed directories' >&2
-    exit 1
+# Test if reprounzip is installed
+if ! type "reprounzip" > /dev/null; then
+    echo ' fatal error: Reprounzip is required.' \
+         ' Install using "pip install reprounzip"' >&2
+    exit 2 
 fi
 
 # Build image with reprozip
-docker run --rm -d --entrypoint='bash' --name='tmp' $INPUT_IMG
-OS=$(docker exec -it tmp 'source /etc/os-release; echo $ID')
+echo "Building image with reprozip..."
+mkdir -p tmp
+docker run --rm -itd --entrypoint='bash' -v ${PWD}/tmp:/reprozip --name='tmp' ${INPUT_IMG}
+docker exec -it tmp bash -c 'source /etc/os-release; echo $ID > /reprozip/os.txt'
+OS=$(cat tmp/os.txt)
 docker stop tmp
 
 CMD_INSTALL=""
-if [[ $OS = "ubuntu" || $OS = "debian" ]]; then
-    CMD_INSTALL='apt-get install -y python python-dev python-pip gcc libsqlite3-dev libssl-dev libffi-dev'
-elif [[ $OS = "fedora" || $OS = "centos" ]]; then
-    CMD_INSTALL='yum install -y python python-devel gcc sqlite-devel openssl-devel libffi-devel'
+PACKAGE_MANAGER=""
+if [ $OS = 'ubuntu' ] || [ $OS = 'debian' ]; then
+    CMD_INSTALL='apt-get -y -q install python python-dev python-pip gcc libsqlite3-dev libssl-dev libffi-dev'
+    PACKAGE_MANAGER='apt-get'
+elif [ $OS = 'fedora' ] || [ $OS = 'centos' ]; then
+    CMD_INSTALL='yum install -y -q python python-devel gcc sqlite-devel openssl-devel libffi-devel'
+    PACKAGE_MANAGER='yum'
 else
-    echo "OS is not supported"
-    return 1
+    echo "$OS is not supported"
+    exit 1
 fi
 
 cat > Dockerfile << EOF
-    FROM $INPUT_IMG
+FROM ${INPUT_IMG}
     
-    RUN $PPACKAGE_MANAGE update \
-    && $CMD_INSTALL
+RUN ${PACKAGE_MANAGER} update -q \
+    && ${CMD_INSTALL}
 
-    RUN pip install -U reprozip
+RUN pip install -U reprozip
 EOF
 
 docker build -t tmp .
 
 # Run the container
 echo "Retrieving file with reprozip..."
-chmod +x $COMMAND_FILE
-docker run -itd --entrypoint="bash" --security-opt=seccomp:unconfined $DOCKER_FLAGS --name="reprozip" tmp
-docker exec -it reprozip bash -c "cd reprozip; reprozip trace --dont-identify-packages sh $COMMAND_FILE; reprozip pack reduced-img"
-docker stop tmp
+cp ${COMMAND_FILE} ./tmp/
+chmod +x ./tmp/${COMMAND_FILE}
+docker run --rm -itd --entrypoint="bash" --security-opt=seccomp:unconfined ${DOCKER_FLAG} -v ${PWD}/tmp:/reprozip --name="reprozip" tmp
+docker exec -it reprozip bash -c "cd reprozip; reprozip trace --dont-identify-packages sh ${COMMAND_FILE}; reprozip pack reduced-img"
+docker stop reprozip
 docker image rm tmp
-echo "Reprozip DONE"
 
+echo "Create reduced image..."
+cd tmp
 reprounzip directory setup reduced-img.rpz reduced-img
+cd ..
 
 cat > Dockerfile << EOF
-    FROM $OS:stable
+FROM $OS
     
-    COPY reduced-img /
+COPY tmp/reduced-img/root /img
+
+RUN rm -r /img/bids_dataset \
+    && rm -r /img/outputs \
+    && rm -r /img/reprozip
 EOF
 
+echo "DONE"
+echo "Remove the tmp folder in the current directory if you want to save space"
